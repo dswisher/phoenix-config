b8d4ef616b0a4f612ab7481d328f1d1b72bbc94f
Doug Swisher <doug.swisher@dowjones.com>
2023-04-02 09:57:39 -0500

    Clean up

--- swish/focus.js
+++ swish/focus.js
@@ -2,20 +2,6 @@
 // Things to help with switching focus between windows
 Swish.Focus = (function()
 {
-    const getDistance = function(winA, winB)
-    {
-        // TODO - this may be overly simplistic - may not handle overlapping windows well
-        let ap = winA.topLeft();
-        let bp = winB.topLeft();
-
-        let dx = ap.x - bp.x;
-        let dy = ap.y = bp.y;
-
-        // The distance is just being used for compares, so no need to take sqrt
-        return dx * dx + dy + dy;
-    }
-
-
     const findNeighbor = function(window, direction)
     {
         // A little debug code and sanity checking
@@ -25,7 +11,7 @@ Swish.Focus = (function()
             return null;
         }
 
-        console.log('Looking for neighbors of ' + window.title() + '...');
+        // console.log('Looking for neighbors of ' + window.title() + '...');
 
         // Get a list of neighbors in the specified direction
         const neighbors = window.neighbors(direction);
@@ -36,70 +22,21 @@ Swish.Focus = (function()
             return null;
         }
 
-        let closestWin = null;
-        let closestDist = 0;
-        neighbors.forEach(neighbor =>
+        for (let i = 0; i < neighbors.length; i++)
         {
+            let neighbor = neighbors[i];
+
             if (neighbor === undefined || !neighbor.isNormal() || !neighbor.isVisible())
             {
-                return;
+                continue;;
             }
 
-            console.log('   ...considering neighbor ' + neighbor.title() + ', app=' + neighbor.app().name() + '...');
-
-            let dist = getDistance(window, neighbor);
-            if (closestWin === null || dist < closestDist)
-            {
-                closestWin = neighbor;
-                closestDist = dist;
-            }
-        });
-
-        if (closestWin)
-        {
-            console.log('   --> Found Closest! app=' + closestWin.app().name() + ', title: ' + closestWin.title());
+            return neighbor;
         }
 
-        return closestWin;
+        return null;
     }
 
-
-    // Based on: https://github.com/Jaredk3nt/phoenix-padding/blob/master/keys/focus.js
-    const findNeighborOld = function(window, direction)
-    {
-        let neighbors = window.neighbors(direction);
-
-        // Find a visible neighbor and give it the focus
-        // TODO - why is this checking for undefined???
-        if (neighbors === undefined || neighbors.length > 0)
-        {
-            let index = 0;
-
-            while (index < neighbors.length)
-            {
-                if (neighbors[index] === undefined)
-                {
-                    return;
-                }
-
-                if (!neighbors[index].isVisible())
-                {
-                    index++;
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            if (neighbors[index] === undefined)
-            {
-                return;
-            }
-
-            return neighbors[index];
-        }
-    }
 
     // Look in the specified direction (north, south, east, west) for a window, and if one is
     // found, move the focus there.
